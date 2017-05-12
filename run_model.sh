8a62b621a3b54d0c5e4eb93e1cee09eb6cb1fb6c
Christian Chamoun <christianchamoun@Christians-MacBook-Air.local>
2017-05-12 12:04:34 +0200

    fixed error in run_model.sh

--- run_model.sh
+++ run_model.sh
@@ -1,4 +1,4 @@
 #Specify image to test from testset folder as input
 read -p "Enter file name with extension: " -e input
 bazel-bin/tensorflow/examples/label_image/label_image --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --output_layer=final_result --input_layer=Mul \
---image=/Users/christianchamoun/Tensorflow-git/tensorflow/testset/AFLW/$input &>> output.txt
+--image=/Users/christianchamoun/Tensorflow-git/tensorflow/testset/AFLW/$input &> output.txt
